1cfae335123162dbe061f5494da8ca87ae3239c2
Apophis <gsapophis@mail.ru>
2013-01-22 13:29:33 +0200

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Shopanvar::Application.config.secret_token = '2ad1c5b2805969c33ebcbf31b33924de67938f4b4cd9322546a92b7e9dcb7a9e3cabd209bcf10d284d7bbf25baec697566cd476ea65a29e132ffef7d2e6e85e9'
